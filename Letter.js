24b785ec8e1078f0296438c734e4ac81fb28c775
Nick.Haer <nhaer123@gmail.com>
2019-08-21 15:27:16 -0500

    added letter file with letter display functionality

--- Letter.js
+++ Letter.js
@@ -1,1 +1,23 @@
 const inquirer = require(`inquirer`)
+
+class Letter {
+    constructor (aLetter) {
+        this.hiddenLetter = aLetter
+        this.guessed = false;
+    }
+
+    displayLetter () {
+        if (this.guessed === true) {
+            return this.hiddenLetter
+        } else {
+            return `_`
+        }
+    }
+
+    checkLetter (guess) {
+        if (guess === this.hiddenLetter) {
+            this.guessed = true;
+        }
+    }
+}
+
